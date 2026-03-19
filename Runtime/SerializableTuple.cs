e0c41f1bd8c03e6203059293b06ad80d1e4bb066
Hunor Csuhai <team3csh@gmail.com>
2026-03-19 21:56:01 +0100

    SerializableTuple UI debug and conversion
    Implicit conversion of SerializableTuple to KeyValuePair.
    Fixed inline height of SerializableTuple in editor.
    Also added a VS-C# specific gitignore.

--- Runtime/SerializableTuple.cs
+++ Runtime/SerializableTuple.cs
@@ -1,44 +1,46 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 namespace NohaSoftware.Utilities
 {
-	/// <summary>
-	/// Equivalent of a <see cref="System.Tuple{T1, T2}"/>
-	/// </summary>
-	[Serializable]
-	public class SerializableTuple<T1, T2>
-	{
-		public T1 Key;
-		public T2 Value;
+    /// <summary>
+    /// Equivalent of a <see cref="System.Tuple{T1, T2}"/>
+    /// </summary>
+    [Serializable]
+    public class SerializableTuple<T1, T2>
+    {
+        public T1 Key;
+        public T2 Value;
 
-		public SerializableTuple(T1 key, T2 value)
-		{
-			Key = key;
-			Value = value;
-		}
+        public SerializableTuple(T1 key, T2 value)
+        {
+            Key = key;
+            Value = value;
+        }
 
-		public override string ToString()
-		{
-			return $"{Key}: {Value}";
-		}
+        public override string ToString()
+        {
+            return $"{Key}: {Value}";
+        }
 
-		public static implicit operator SerializableTuple<T1, T2>(Tuple<T1, T2> tuple)
-		{
-			return new SerializableTuple<T1, T2>(tuple.Item1, tuple.Item2);
-		}
-		public static implicit operator Tuple<T1, T2>(SerializableTuple<T1, T2> serializableTuple)
-		{
-			return new Tuple<T1, T2>(serializableTuple.Key, serializableTuple.Value);
-		}
-		public static explicit operator KeyValuePair<T1, T2>(SerializableTuple<T1, T2> serializableTuple)
-		{
-			return new KeyValuePair<T1, T2>(serializableTuple.Key, serializableTuple.Value);
-		}
-	}
+        public static implicit operator SerializableTuple<T1, T2>(Tuple<T1, T2> tuple)
+        {
+            return new SerializableTuple<T1, T2>(tuple.Item1, tuple.Item2);
+        }
+        public static implicit operator Tuple<T1, T2>(SerializableTuple<T1, T2> serializableTuple)
+        {
+            return new Tuple<T1, T2>(serializableTuple.Key, serializableTuple.Value);
+        }
+        public static implicit operator KeyValuePair<T1, T2>(SerializableTuple<T1, T2> serializableTuple)
+        {
+            return new KeyValuePair<T1, T2>(serializableTuple.Key, serializableTuple.Value);
+        }
+    }
 
 #if UNITY_EDITOR
 	[CustomPropertyDrawer(typeof(SerializableTuple<,>))]
@@ -52,38 +54,47 @@ namespace NohaSoftware.Utilities
 			position = EditorGUI.IndentedRect(position);
 
 			// Calculate the height of each element in the property
-			float lineHeight = EditorGUIUtility.singleLineHeight;
+			float keyHeight = EditorGUI.GetPropertyHeight(property.FindPropertyRelative("Key"), includeChildren: true);
+			float valueHeight = EditorGUI.GetPropertyHeight(property.FindPropertyRelative("Value"), includeChildren: true);
 			float spacing = EditorGUIUtility.standardVerticalSpacing;
 
 			// Draw the key and value fields
-			EditorGUI.PropertyField(new Rect(position.x, position.y, position.width / 2 - spacing, lineHeight), property.FindPropertyRelative("Key"), GUIContent.none);
-			EditorGUI.PropertyField(new Rect(position.x + position.width / 2, position.y, position.width / 2 - spacing, lineHeight), property.FindPropertyRelative("Value"), GUIContent.none);
+			EditorGUI.PropertyField(new Rect(position.x, position.y, position.width / 2 - spacing, keyHeight), property.FindPropertyRelative("Key"), GUIContent.none);
+			EditorGUI.PropertyField(new Rect(position.x + position.width / 2, position.y, position.width / 2 - spacing, valueHeight), property.FindPropertyRelative("Value"), GUIContent.none);
 
 			EditorGUI.EndProperty();
+		}
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			float keyHeight = EditorGUI.GetPropertyHeight(property.FindPropertyRelative("Key"), includeChildren: true);
+			float valueHeight = EditorGUI.GetPropertyHeight(property.FindPropertyRelative("Value"), includeChildren: true);
+			float lineHeight = Mathf.Max(EditorGUIUtility.singleLineHeight, keyHeight);
+			lineHeight = Mathf.Max(lineHeight, valueHeight);
+			return lineHeight;
 		}
 	}
 #endif
 
-	public static class SerializableTupleExtensions
-	{
-		public static SerializableTuple<T1, T2> GetElement<T1, T2>(this IEnumerable<SerializableTuple<T1, T2>> serializableTuples, T1 key) where T1 : IEquatable<T1>
-		{
-			var query = serializableTuples.Where(sTuple => sTuple.Key.Equals(key));
-			if (query.Count() > 0) return query.First();
-			else return null;
-		}
-		public static bool ContainsKey<T1, T2>(this IEnumerable<SerializableTuple<T1, T2>> serializableTuples, T1 key) where T1 : IEquatable<T1>
-		{
-			return serializableTuples.GetElement(key) != null;
-		}
+    public static class SerializableTupleExtensions
+    {
+        public static SerializableTuple<T1, T2> GetElement<T1, T2>(this IEnumerable<SerializableTuple<T1, T2>> serializableTuples, T1 key) where T1 : IEquatable<T1>
+        {
+            var query = serializableTuples.Where(sTuple => sTuple.Key.Equals(key));
+            if (query.Count() > 0) return query.First();
+            else return null;
+        }
+        public static bool ContainsKey<T1, T2>(this IEnumerable<SerializableTuple<T1, T2>> serializableTuples, T1 key) where T1 : IEquatable<T1>
+        {
+            return serializableTuples.GetElement(key) != null;
+        }
 
-		public static IEnumerable<SerializableTuple<T1, T2>> SerializeDictionary<T1, T2>(this Dictionary<T1, T2> dictionary)
-		{
-			return dictionary.Select(pair => new SerializableTuple<T1, T2>(pair.Key, pair.Value));
-		}
-		public static Dictionary<T1, T2> DeserializeDictionary<T1, T2>(this IEnumerable<SerializableTuple<T1, T2>> serializedDictionary)
-		{
-			return serializedDictionary.ToDictionary(t => t.Key, t => t.Value);
-		}
-	}
+        public static IEnumerable<SerializableTuple<T1, T2>> SerializeDictionary<T1, T2>(this Dictionary<T1, T2> dictionary)
+        {
+            return dictionary.Select(pair => new SerializableTuple<T1, T2>(pair.Key, pair.Value));
+        }
+        public static Dictionary<T1, T2> DeserializeDictionary<T1, T2>(this IEnumerable<SerializableTuple<T1, T2>> serializedDictionary)
+        {
+            return serializedDictionary.ToDictionary(t => t.Key, t => t.Value);
+        }
+    }
 }
